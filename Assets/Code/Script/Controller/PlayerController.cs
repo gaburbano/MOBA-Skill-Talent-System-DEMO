294cc2b04b66ce17e1e8550475f76cbce73b5809
GAB <jgaburbano@gmail.com>
2023-05-08 03:47:32 +0800

    Implement initial movement for player

--- Assets/Code/Script/Controller/PlayerController.cs
+++ Assets/Code/Script/Controller/PlayerController.cs
@@ -4,89 +4,84 @@ public class PlayerController : MonoBehaviour {
     public Skill[] skills;
     public Talent[] talents;
 
-    // Public variables for player movement and targeting
     public float moveSpeed = 5.0f;
+    public float stopDistance = 0.1f;
     public float attackRange = 2.0f;
     public float attackCooldown = 1.0f;
     public Transform target;
 
-    // Private variables for player state and attacking
     private bool isMoving = false;
     private bool isAttacking = false;
     private float attackTimer = 0.0f;
 
-    void Update()
-    {
-        // Check if player has a target
-        if (target != null)
-        {
-            // Move towards the target
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Vector2 movementTargetPosition;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            movementTargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if ((Vector2)transform.position != movementTargetPosition) {
+            transform.position = Vector2.MoveTowards(transform.position, movementTargetPosition, moveSpeed * Time.deltaTime);
+        }
+
+        if (target != null) {
             Vector3 direction = target.position - transform.position;
             transform.Translate(direction.normalized * moveSpeed * Time.deltaTime, Space.World);
 
-            // Check if player is in attack range
-            if (direction.magnitude <= attackRange)
-            {
-                // Check if attack is off cooldown
-                if (!isAttacking && Time.time > attackTimer)
-                {
-                    // Start attacking
+            if (direction.magnitude <= attackRange) {
+                if (!isAttacking && Time.time > attackTimer) {
                     isMoving = false;
                     isAttacking = true;
                     attackTimer = Time.time + attackCooldown;
 
-                    // Play attack animation and deal damage to target
-                    // Replace this code with your own attack animation and damage logic
-                    if (GetComponent<Animator>() != null)
-                    {
+                    if (GetComponent<Animator>() != null) {
                         GetComponent<Animator>().SetTrigger("Attack");
                     }
                     target.GetComponent<Health>().TakeDamage(10);
                 }
-            }
-            else
-            {
-                // Move towards the target
+            } else {
                 isMoving = true;
                 isAttacking = false;
             }
-        }
-        else
-        {
-            // Stop moving and attacking if no target
+        } else {
             isMoving = false;
             isAttacking = false;
         }
 
-        // Update player animation state
         Animator animator = GetComponent<Animator>();
-        if (animator != null)
-        {
+        if (animator != null) {
             animator.SetBool("IsMoving", isMoving);
             animator.SetBool("IsAttacking", isAttacking);
         }
     }
 
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (Vector2.Distance(transform.position, movementTargetPosition) < stopDistance) {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
     void UseSkill(int skillIndex) {
         Skill skill = skills[skillIndex];
-        // apply all skill effects to target(s)
         foreach (SkillEffect effect in skill.effects) {
-            // apply effect
             if (effect.type == SkillEffectType.DAMAGE) {
                 // apply damage to target
-                // ...
             } else if (effect.type == SkillEffectType.HEAL) {
                 // apply healing to target
-                // ...
             } else if (effect.type == SkillEffectType.BUFF) {
                 // apply buff to target
-                // ...
             }
-            // play VFX, if applicable
             if (effect.vfx != null) {
-                // spawn particle effect
                 GameObject vfxObj = Instantiate(effect.vfx.particleEffect, target.position, Quaternion.identity);
-                // destroy particle effect after duration
                 Destroy(vfxObj, effect.vfx.duration);
             }
         }
@@ -95,20 +90,15 @@ public class PlayerController : MonoBehaviour {
     void ApplyTalent(int talentIndex, int skillIndex) {
         Talent talent = talents[talentIndex];
         Skill targetSkill = skills[skillIndex];
-        // apply all talent modifiers to target skill
         foreach (TalentModifier modifier in talent.modifiers) {
             if (modifier.targetSkill == targetSkill) {
-                // modify skill attribute
                 if (modifier.modificationType == "damage") {
                     targetSkill.damage += modifier.modificationValue;
                 } else if (modifier.modificationType == "manaCost") {
                     targetSkill.manaCost -= modifier.modificationValue;
                 }
-                // play VFX, if applicable
                 if (modifier.vfx != null) {
-                    // spawn particle effect
                     GameObject vfxObj = Instantiate(modifier.vfx.particleEffect, target.position, Quaternion.identity);
-                    // destroy particle effect after duration
                     Destroy(vfxObj, modifier.vfx.duration);
                 }
             }
